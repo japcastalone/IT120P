83bfa49208ffbafd67be98686dc6d7127f0d206a
japcastalone <japcastalone@mymail.mapua.edu.ph>
2023-05-01 10:47:26 +0800

    revised

--- Controllers/DeviceController.cs
+++ Controllers/DeviceController.cs
@@ -10,11 +10,13 @@ namespace IT120P.Controllers
     {
         private readonly DeviceDbContext _dbContext;
         private readonly AuthService _authService;
+        private readonly UserService _userService;
 
-        public DeviceController(DeviceDbContext dbContext, AuthService authService)
+        public DeviceController(DeviceDbContext dbContext, AuthService authService, UserService userService)
         {
             _dbContext = dbContext;
             _authService = authService;
+            _userService = userService;
         }
 
         [HttpPost]
@@ -31,44 +33,49 @@ namespace IT120P.Controllers
             }
         }
 
-        [HttpPost]
-        [Route("")]
+        [HttpPost("devices")]
         public IActionResult AddDevice([FromBody] Devices device)
         {
-            if (_authService.Authenticate(HttpContext.User.Identity.Name, HttpContext.Request.Headers["Authorization"]))
-            {
-                _dbContext.Devices.Add(device);
-                _dbContext.SaveChanges();
-                return Ok();
-            }
-            else
+            // get user id from token
+            int userId = int.Parse(User.Identity.Name);
+
+            // check if user has sufficient authority
+            var user = _userService.GetById(userId);
+            if (user.Autho != "admin")
             {
                 return Unauthorized();
             }
+
+            // add device to database
+            _dbContext.Devices.Add(device);
+            _dbContext.SaveChanges();
+
+            return Ok(device);
         }
 
-        [HttpDelete]
-        [Route("{id}")]
+        [HttpDelete("devices/{id}")]
         public IActionResult DeleteDevice(int id)
         {
-            if (_authService.Authenticate(HttpContext.User.Identity.Name, HttpContext.Request.Headers["Authorization"]))
-            {
-                var device = _dbContext.Devices.SingleOrDefault(d => d.Id == id);
-                if (device != null)
-                {
-                    _dbContext.Devices.Remove(device);
-                    _dbContext.SaveChanges();
-                    return Ok();
-                }
-                else
-                {
-                    return NotFound();
-                }
-            }
-            else
+            // get user id from token
+            int userId = int.Parse(User.Identity.Name);
+
+            // check if user has sufficient authority
+            var user = _userService.GetById(userId);
+            if (user.Autho != "admin")
             {
                 return Unauthorized();
             }
+
+            // delete device from database
+            var device = _dbContext.Devices.Find(id);
+            if (device == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Devices.Remove(device);
+            _dbContext.SaveChanges();
+
+            return Ok();
         }
     }
 
---
+++ Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using IT120P.Models;
+
+namespace IT120P.Services
+{
+    public class UserService
+    {
+        private readonly DeviceDbContext _dbContext;
+
+        public UserService(DeviceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public User Authenticate(string username, string password)
+        {
+            var user = _dbContext.Users.SingleOrDefault(x => x.Username == username && x.Password == password);
+
+            // return null if user not found
+            if (user == null)
+            {
+                return null;
+            }
+
+            // remove password before returning
+            user.Password = null;
+
+            return user;
+        }
+
+    }
+}
